caaffa284b0816dbd2ac57f4f4df1da290753cbf
JeremyMarcello <inijemaryt@gmail.com>
2024-04-01 15:29:39 +0700

    Push Laprak

--- Modul 3/Unguided/Unguided-1.cpp
+++ Modul 3/Unguided/Unguided-1.cpp
@@ -1,41 +1,39 @@
 #include <iostream>
 using namespace std;
 
-void insertion_sort(float arr[], int panjang) {
-    int i, j;
-    float n;
-
-    for (i = 1; i < panjang; i++) {
-        j = i;
-
-        while (j > 0 && arr[j - 1] < arr[j]) {
-            n = arr[j];
-            arr[j] = arr[j - 1];
-            arr[j - 1] = n;
-            j--;
-        }
+void selection_sort(float ips[], int panjang){
+  for (int i = 0; i < panjang - 1; i++) {
+    int min_index = i;
+    for (int j = i + 1; j < panjang; j++) {
+      if (ips[min_index] < ips[j]) {
+        min_index = j;
+      }
     }
+    float tmp = ips[i];
+    ips[i] = ips[min_index];
+    ips[min_index] = tmp;
+}
 }
 
-void print_array(float a[], int panjang) {
+void print_array(float ips[], int panjang) {
 
     for(int i=0; i<panjang; i++) {
-        cout << a[i] << "\t";
+        cout << ips[i] << "\t";
     }
     cout << endl;
 }
 
-int main () {
-    int panjang = 5;
-    float a[panjang] = {3.8, 2.9, 3.3, 4.0, 2.4};
+int main() {
+  float ips_mhs[] = {3.8, 2.9, 3.3, 4.0, 2.4};
+  int n = sizeof(ips_mhs) / sizeof(ips_mhs[0]);
 
     cout << "Urutan IPS sebelum sorting: " << endl;
-    print_array(a, panjang);
+    print_array(ips_mhs, n);
 
-    insertion_sort(a, panjang);
+    selection_sort(ips_mhs, n);
 
     cout << "\nUrutan IPS setelah sorting: " << endl;
-    print_array(a, panjang);
+    print_array(ips_mhs, n);
 
- 
+  return 0;
 }
